4f8d2fb2c764389890d9da0cb5e88bd712ceaad4
Самсонов Евгений Юрьевич <ESamsonov@alfabank.ru>
2025-01-11 20:21:35 +0500

    minor fixes

--- constants.py
+++ constants.py
@@ -1,9 +1,10 @@
 from dotenv import load_dotenv
 import os
 
-BOT_TIMEZONE = 5
 
 load_dotenv()
+
+BOT_TIMEZONE = 5
 TOKEN = os.getenv('TOKEN')
 SQLITE_FILE = os.getenv('SQLITE_FILE')
 WEATHER_SERVICE_URL = 'https://worldweather.wmo.int/ru/json/{city_id}_ru.xml'
